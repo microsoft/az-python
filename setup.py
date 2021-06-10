8c20297f83eaaa3a7583309c3a13c53d248d0f6a
Daniel Ciborowski <dciborow@microsoft.com>
2021-06-10 18:28:05 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -251,7 +251,6 @@ setup(
         "Topic :: Scientific/Engineering :: Artificial Intelligence"],
     python_requires=">=3.7,<3.8",
     setup_requires=SETUP_REQUIRES,
-    install_requires=[],
     extra_requires=extras,
     packages=find_packages(exclude=["tests", "tests.*", "examples", "examples.*"]),
     include_package_data=True,
