cd9e9f048314b664cfcff5445739819d815614c4
Daniel Ciborowski <dciborow@microsoft.com>
2022-05-19 13:18:39 -0400

    Update __init__.py

--- src/az-python/__init__.py
+++ src/az-python/__init__.py
@@ -1,1 +1,6 @@
+#   -------------------------------------------------------------
+#   Copyright (c) Microsoft Corporation. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   -------------------------------------------------------------
+"""Collection of Python Libraries for use on Azure"""
 __version__="1.0.2"
